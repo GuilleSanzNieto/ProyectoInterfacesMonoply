b2b088ec923f3fc2e01d2ba6ce450def3df04b2c
martavegas02 <martitavegas286@gmail.com>
2025-03-18 19:02:54 +0100

    commonStyles.css prueba

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,14 @@ import Tablero from './components/Tablero.jsx'
 import TicTacToe from './juegos/TicTacToe.jsx'
 import WordSearch from './juegos/WordSearch.jsx'
 import MemoriCard from './juegos/MemoriCard.jsx'
-import Sudoku from './juegos/Sudoku.jsx'
+//import Sudoku from './juegos/Sudoku.jsx'
 import Conecta4 from './juegos/Conecta4.jsx'
+import MatesTest from './juegos/MatesTest.jsx'
+import PiedraPapelTijera from './juegos/PiedraPapelTijera.jsx'
+import Trivial from './juegos/Trivial.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Conecta4 />
+    <WordSearch />
   </StrictMode>
 )
