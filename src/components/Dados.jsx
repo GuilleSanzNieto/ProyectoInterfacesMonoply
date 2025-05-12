d4e5226fdba6f7d0a133d2e3867642336b63a2e1
martavegas02 <martitavegas286@gmail.com>
2025-05-12 11:36:17 +0200

    Color de la casilla sin moverse, pero no se ve si se para en la fila superior

--- src/components/Dados.jsx
+++ src/components/Dados.jsx
@@ -27,8 +27,8 @@ const Dados = ({ spinning, setValor, index }) => {
   };
 
   const spin = () => {
-    // const randomNum = Math.floor(Math.random() * 6) + 1;
-    const randomNum = 1;
+    const randomNum = Math.floor(Math.random() * 6) + 1;
+    //const randomNum = 1;
     
     let newKindaSpin = ''; 
     let newA = ''; 
